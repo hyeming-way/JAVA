ca7f53bbdd94d8331ee047d978a044536b0d4264
hyemi <gpal7355@naver.com>
2025-02-24 17:15:47 +0900

    *자바(01자료형) - 상수리터럴 종류_자료형의 역할

---
+++ 01자료형/src/Data02.java
@@ -0,0 +1,15 @@
+
+//예제 : 더 다양한 종류의 상수리터럴 출력해보기 2
+public class Data02 {
+
+	public static void main(String[] args) {
+		//(1) long형 정수 형태의 상수 : 숫자 끝에 L 혹은 l 접미사를 붙여 표현한 정수
+		System.out.println(1L); //1
+		//(2) float형 실수 형태의 상수 : 숫자 끝에 F 혹은 f 접미사를 붙여 표현한 실수
+		System.out.println(1.5f); //1.5
+		//(3) 레퍼런스형 중에서 문자열 상수 : 이중 따옴표 ""로 묶어준 데이터
+		System.out.println("abc"); //abc
+			
+	}
+
+}
